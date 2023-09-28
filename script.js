ff1fb7d13de3b812ba63312eb81625cc6fb24a28
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-28 15:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const browser = document.getElementById("browser-info");
+
+let browserName = window.navigator.appName;
+let browserVersion = window.navigator.appVersion;
+
+let details = "You are using " + browserName + " version " + browserVersion;
+
+
+browser.innerHTML = details;
